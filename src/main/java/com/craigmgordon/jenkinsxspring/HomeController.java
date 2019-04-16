8dbc0497f864459aa272479cec6d41c9083b4987
Craig Gordon <craigmgordon@gmail.com>
2019-04-16 19:51:23 +0100

    add a comment

--- src/main/java/com/craigmgordon/jenkinsxspring/HomeController.java
+++ src/main/java/com/craigmgordon/jenkinsxspring/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+	//add a comment
 	@GetMapping(value="/home")
 	public String test() {
 		return "This is mhy test controller";
